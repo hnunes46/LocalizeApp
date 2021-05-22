4f0f9f4426c8a51065ea4007594df1f258dc7c5e
Helder Nunes <helder.nunes46@gmail.com>
2021-05-22 09:50:32 +0100

    Initial Commit

---
+++ LocalizeApp/LocalizeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizeAppApp.swift
+//  LocalizeApp
+//
+//  Created by Helder Nunes on 22/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct LocalizeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
